64c5dc9af36dcd1cbc7f49899b2ddc1724767b9b
zo-el <joelulahanna@gmail.com>
2025-02-05 13:25:44 -0600

    release v0.2.3

--- kangaroo.config.ts
+++ kangaroo.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from './src/main/defineConfig';
 export default defineConfig({
   appId: 'co.unyt.hfvz',
   productName: 'hfvz',
-  version: '0.2.2',
+  version: '0.2.3',
   macOSCodeSigning: false,
   windowsEVCodeSigning: false,
   fallbackToIndexHtml: true,
